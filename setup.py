1abc732cac956257bbc4ef92d361f08839210dda
Jordan Heemskerk <jordanh@shaw.ca>
2016-08-07 12:30:26 -0700

    Add setup.py and blank requirement.txt

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='fantasy-analysis',
+      version='0.1',
+      py_modules=['fantasyanalysis'],
+      description='A NFL Fantasy Analysis Tool',
+      author='Jordan Heemskerk',
+      author_email='jordanh@shaw.ca',
+      )
